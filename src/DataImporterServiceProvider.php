98e6ed161dc43bed56653b3f553db6ff280100c9
Behzad Shabani <behzad.shabani@gmail.com>
2014-12-18 20:16:45 +0330

    Add package service provider

---
+++ src/DataImporterServiceProvider.php
@@ -0,0 +1,40 @@
+<?php namespace Quince\DataImporter;
+
+use Illuminate\Support\ServiceProvider;
+
+class DataImporterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('quince/data-importer');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['config']->package('quince/data-importer', __DIR__ . '/config');
+
+		$this->app->alias('importer', DataImporterManager::class);
+
+		$this->app->bind('importer', function ($app) {
+			return new DataImporterManager(
+				$app,
+				$app['config']
+			);
+		});
+
+		$this->app->bind('Quince\Contracts\DataImporter\Importer', function ($app) {
+			return $app['exporter'];
+		});
+	}
+
+}
