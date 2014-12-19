db7475dc388b8202da3fa597e018fac373ecfcd7
Behzad Shabani <behzad.shabani@gmail.com>
2014-12-19 12:48:27 +0330

    Implement ArrayAccess interface in dictionary data object

--- src/DataObjects/Dictionary.php
+++ src/DataObjects/Dictionary.php
@@ -1,6 +1,6 @@
-<?php namespace Quince\DataImporter\DataObjects; 
+<?php namespace Quince\DataImporter\DataObjects;
 
-class Dictionary {
+class Dictionary implements \ArrayAccess {
 
 	protected $terms = [];
 
@@ -22,6 +22,55 @@ class Dictionary {
 	public function setTermTranslation($term, $translation)
 	{
 		$this->terms[$term] = $translation;
+	}
+
+	/**
+	 * Whether a offset exists
+	 *
+	 * @param mixed $offset
+	 * @return boolean
+	 */
+	public function offsetExists($offset)
+	{
+		return isset($this->terms[$offset]);
+	}
+
+	/**
+	 * Offset to retrieve
+	 *
+	 * @param mixed $offset
+	 * @return mixed
+	 */
+	public function offsetGet($offset)
+	{
+		return isset($this->terms[$offset]) ? $this->terms[$offset] : null;
+	}
+
+	/**
+	 * Offset to set
+	 *
+	 * @param mixed $offset
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function offsetSet($offset, $value)
+	{
+		if (is_null($offset)) {
+			throw new \InvalidArgumentException('Invalid terms to set translation for');
+		}
+
+		$this->terms[$offset] = $value;
+	}
+
+	/**
+	 * Offset to unset
+	 *
+	 * @param mixed $offset
+	 * @return void
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->terms[$offset]);
 	}
 
 }
