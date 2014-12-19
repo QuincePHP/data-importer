e4af29c8c69a06f3f78664a6405e9b355bae23e8
Behzad Shabani <behzad.shabani@gmail.com>
2014-12-19 12:32:46 +0330

    Add data object factory

---
+++ src/DataObjects/DataObjectFactory.php
@@ -0,0 +1,26 @@
+<?php namespace Quince\DataImporter\DataObjects;
+
+use Illuminate\Support\Str;
+use ReflectionClass;
+
+class DataObjectFactory {
+
+	/**
+	 * @param string $dataObjectName
+	 * @param array  $parameters
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public static function make($dataObjectName, $parameters = [])
+	{
+		$class = __NAMESPACE__ . ucfirst(Str::camel($dataObjectName));
+
+		if (!class_exists($class)) {
+			throw new \Exception('Cannot find DataObject ' . $class);
+		}
+
+		$class = new ReflectionClass($class);
+		return $class->newInstanceArgs($parameters);
+	}
+
+}
