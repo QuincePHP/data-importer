24d92281437bd508cbd793fa181155f39370263b
Behzad Shabani <behzad.shabani@gmail.com>
2015-02-18 15:50:43 +0330

    Test importing csv with extra fields

--- tests/DataImporterManagerTest.php
+++ tests/DataImporterManagerTest.php
@@ -59,6 +59,49 @@ class DataImporterManagerTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals(ceil(10000/$this->getChunkSize()), $calledTime);
 	}
 
+	public function testImportingSimpleCsvWithExtraHeaders()
+	{
+		// constant variables
+		$model = 'SampleModel';
+		$tableName = 'model_table';
+		$filePath = __DIR__ . '/data/extra-columns.csv';
+		$tableColumn = ['username', 'password', 'email'];
+
+		/**
+		 * @var Mockery\MockInterface $app
+		 * @var Mockery\MockInterface $config
+		 */
+		list($app, $config) = $this->mockObjects($model, $tableName, $tableColumn);
+
+		$calledTime = 0;
+
+		$this->getSut($app, $config)->import($filePath, $model, function ($data) use (&$calledTime) {
+
+			/** @var \Quince\DataImporter\DataObjects\RowsCollection $data */
+			$this->assertInstanceOf('\Quince\DataImporter\DataObjects\RowsCollection', $data);
+			$this->assertLessThanOrEqual($this->getChunkSize(), $data->count());
+
+			/** @var \Quince\DataImporter\DataObjects\RowData $row */
+			foreach ($data as $row) {
+				$this->assertInstanceOf('\Quince\DataImporter\DataObjects\RowData', $row);
+				$this->assertInstanceOf('\Quince\DataImporter\DataObjects\RelationData', $row->getRelation());
+				$this->assertTrue($row->getRelation()->isEmpty());
+			}
+
+			foreach ($data->toArray() as $value) {
+				$this->assertArrayNotHasKey('name', $value['base']);
+				$this->assertArrayHasKey('username', $value['base']);
+				$this->assertArrayHasKey('email', $value['base']);
+				$this->assertArrayHasKey('password', $value['base']);
+				$this->assertEmpty($value['relation']);
+			}
+
+			$calledTime++;
+		});
+
+		$this->assertEquals(ceil(10000/$this->getChunkSize()), $calledTime);
+	}
+
 	/**
 	 * @param $model
 	 * @param $tableName
