8db376eaf1ffcbc0178b18d06c98ed9f3daa8e4f
Behzad Shabani <behzad.shabani@gmail.com>
2015-02-22 13:54:25 +0330

    Implement ArrayAccess in RowData data object

--- src/DataObjects/RowData.php
+++ src/DataObjects/RowData.php
@@ -1,8 +1,9 @@
 <?php namespace Quince\DataImporter\DataObjects;
 
+use ArrayAccess;
 use Illuminate\Support\Contracts\ArrayableInterface;
 
-class RowData implements ArrayableInterface {
+class RowData implements ArrayableInterface, ArrayAccess {
 
 	/**
 	 * @var array
@@ -73,6 +74,77 @@ class RowData implements ArrayableInterface {
 		}
 
 		return $tmp;
+	}
+
+	/**
+	 * Whether a offset exists
+	 *
+	 * @param mixed $offset
+	 * @return boolean
+	 */
+	public function offsetExists($offset)
+	{
+		if (strtolower($offset) == 'base' || strtolower($offset) == 'relation') {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Offset to retrieve
+	 *
+	 * @param mixed $offset
+	 * @return mixed
+	 */
+	public function offsetGet($offset)
+	{
+		if ($this->offsetExists($offset)) {
+			return call_user_func([$this, 'get' . camel_case(strtolower($offset)) . 'Array']);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Offset to set
+	 *
+	 * @param mixed $offset
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function offsetSet($offset, $value)
+	{
+		return false;
+	}
+
+	/**
+	 * Offset to unset
+	 *
+	 * @param mixed $offset
+	 * @return void
+	 */
+	public function offsetUnset($offset)
+	{
+		if ($this->offsetExists($offset)) {
+			unset($this->$offset);
+		}
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getBaseArray()
+	{
+		return $this->base;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getRelationArray()
+	{
+		return $this->relation->toArray();
 	}
 
 }
